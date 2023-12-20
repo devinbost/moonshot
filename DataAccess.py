15651c4d5d13d17ea364f441c31934a553246a74
Devin Bost <devin.bost@gmail.com>
2023-12-20 12:50:40 -0600

    Cleaned up more

--- DataAccess.py
+++ DataAccess.py
@@ -6,6 +6,7 @@ from cassandra.auth import PlainTextAuthProvider
 import os
 import json
 
+from graphviz import Digraph
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import Cassandra
@@ -14,6 +15,7 @@ import pandas as pd
 from langchain.docstore.document import Document
 
 import config
+from ComponentData import ComponentData
 
 
 class DataAccess:
@@ -25,7 +27,7 @@ class DataAccess:
         #     model_name="all-mpnet-base-v2"
         # )
         self.output_variables = ["new", "myllm", "emb"]
-        self.dag = {}
+        self.data_map = {}
         self.embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
         self.splitter = RecursiveCharacterTextSplitter(
             chunk_size=4000,
@@ -98,3 +100,43 @@ class DataAccess:
 
     def add_output_variable(self, variable):
         self.output_variables.append(variable)
+
+    def add_component(self, component_data: ComponentData):
+        self.data_map[component_data.id] = component_data
+        if component_data.output_var is not None:
+            self.add_output_variable(component_data.output_var)
+
+    def get_data_map(self):
+        return self.data_map
+
+    def build_graph(self, component_dict: dict[str, ComponentData], graph: Digraph):
+        # Style for nodes
+        graph.attr(
+            "node",
+            shape="box",
+            style="rounded,filled",
+            fillcolor="lightgrey",
+            fontname="Helvetica",
+        )
+
+        # Add nodes with additional attributes
+        for left_id, left_data in component_dict.items():
+            label = f"{left_data.component_name} | Class: {left_data.class_name} | Library: {left_data.library} | Access: {left_data.access_type} | Params: {left_data.params} | Output: {left_data.output_var}"
+            graph.node(left_id, label=label)
+            print(f"Adding graph.node({left_id}, label={label})")
+
+        # Add edges
+        for left_id, left_data in component_dict.items():
+            for right_id, right_data in component_dict.items():
+                if left_id == right_id:
+                    continue
+                if left_data.output_var in right_data.params.values():
+                    left_label = f"{left_data.component_name} | Class: {left_data.class_name} | Library: {left_data.library} | Access: {left_data.access_type} | Params: {left_data.params}"
+                    right_label = f"{right_data.component_name} | Class: {right_data.class_name} | Library: {right_data.library} | Access: {right_data.access_type} | Params: {right_data.params}"
+                    print(f"left_label is {left_label})")
+                    print(f"right_label is {right_label})")
+                    graph.edge(left_id, right_id, label=left_data.output_var)
+                    print(
+                        f"Adding graph.edge({left_id}, {right_id}, label={left_data.output_var})"
+                    )
+        return graph
